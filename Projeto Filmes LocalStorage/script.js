36d0c8fae5029900011c2989df5d1c8cac3e5e68
Caio Oliveira <caio.oliveira07@gmail.com>
2024-03-08 02:21:56 -0300

    Adicionar e remover do localStorage

--- Projeto Filmes LocalStorage/script.js
+++ Projeto Filmes LocalStorage/script.js
@@ -4,34 +4,34 @@ Criando a base de dados de filmes
 
 const filmes = [
     {
+    id: 0,
     nome: 'Harry Potter',
     genero: 'fantasia',
-    lancamento: 2001,
-    isFavorite:false
+    lancamento: 2001
     },
     {
+    id: 1,
     nome: 'Avatar',
     genero: 'fantasia',
-    lancamento: 2010,
-    isFavorite:false
+    lancamento: 2010
     },
     {
+     id:2,
      nome:'O senhor dos Anéis',
      genero: 'fantasia',
      lancamento: 2000,
-     isFavorite:false
     },
     {
+     id:3,
      nome: 'Branquelas',
      genero: 'comédia',
-     lancamento: 2007,
-     isFavorite:false
+     lancamento: 2007
     },
     {
+     id:4,
      nome: 'A Lagoa Azul',
      genero: 'romance',
-     lancamento: 1983,
-     isFavorite:false
+     lancamento: 1983
 }]
 
 /*
@@ -95,8 +95,12 @@ Adiciona o evento de clique ao botão
 btn1.addEventListener('click',()=>{
     //pega o input onde o usuário digita o filme
     const inputUsuario = document.querySelector('#filmeInput')
+    /*adiciona um id ao filme considerando que o tamanho do array
+    será sempre um a mais que seu index, já que o index começa em 0 */
+    let id = filmes.length
     //adiciona o valor à propriedade nome do objeto dentro do array filmes
-    filmes.push({nome: inputUsuario.value, genero: '', lancamento: ''})
+    filmes.push({id:id,nome: inputUsuario.value, genero: '', lancamento: ''})
+    console.log(filmes)
     //renderiza a lista novamente
     renderizarLista()
     //apaga o campo de digitação
@@ -104,19 +108,22 @@ btn1.addEventListener('click',()=>{
 })
 
 const favoritoClicado = (eventoDeClique, objetoFilme) => {
+    /*adiciona um objeto com a propriedade favorito e não favorito,
+    e seus valores são os caminhos da imagem*/
     const favoriteState = {
         favorited: 'img/heart-fill.svg',
         notFavorited: 'img/heart.svg'
       }
-
+      //valida se o src da imagem que foi clicada inclui o caminho da imagem de não favoritado
       if(eventoDeClique.target.src.includes(favoriteState.notFavorited)) {
-        // aqui ele será favoritado
+        // se não incluir, mudar a imagem para favoritado e executar a função de salvar no localStorage
         eventoDeClique.target.src = favoriteState.favorited
-        //saveToLocalStorage(objetoFilme)
+        saveToLocalStorage(objetoFilme)
       } else {
-        // aqui ele será desfavoritado
+        /* senão, manter a imagem de não favoritado e executar a função de remover
+        do localStorage, passando como parâmetro o id do filme*/
         eventoDeClique.target.src = favoriteState.notFavorited
-        //removeFromLocalStorage(objetoFilme.id)
+        removeFromLocalStorage(objetoFilme.id)
       }
 
 }
@@ -128,12 +135,26 @@ const saveToLocalStorage = (objetoFilme) => {
         filmesFavoritos = JSON.parse(localStorage.getItem('favoritos'));
     }
     //adiciona o nome do Filme ao array filmesFavoritos
-    filmesFavoritos.push(objetoFilme.nome)
+    filmesFavoritos.push(objetoFilme)
     //transforma o array em string para poder salvar no LocalStorage
     const moviesJSON = JSON.stringify(filmesFavoritos)
     //Salva no localStorage
     localStorage.setItem('favoritos', moviesJSON)
 }
+
+function removeFromLocalStorage(id) {
+     //checa se já existe um campo de favoritos no LocalStorage
+    //se houver, ele salva no array filmesFavoritos
+    if(localStorage.getItem('favoritos')){
+        filmesFavoritos = JSON.parse(localStorage.getItem('favoritos'));
+    }
+    //procura no array o id do filme
+    const procurarFilme = filmesFavoritos.find(movie => movie.id === id)
+    //filtra todos os filmes que tem o id diferente do que foi encontrado e gera um novo array
+    const filmesFiltrados = filmesFavoritos.filter(movie => movie.id != procurarFilme.id)
+    //guarda esse novo array no localStorage
+    localStorage.setItem('favoritos', JSON.stringify(filmesFiltrados))
+  }
 
 
 /*
